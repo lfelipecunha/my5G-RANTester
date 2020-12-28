035bdaf68bb071d633c50afc5a90fc8089c229de
Luiz Felipe Cunha <felipe.silvacunha@gmail.com>
2020-12-28 14:53:13 -0300

    Corrected definition of test type

--- internal/templates/test-multi-attach-ue-stress-load.go
+++ internal/templates/test-multi-attach-ue-stress-load.go
@@ -1,105 +1,104 @@
 package templates
 
 import (
-    log "github.com/sirupsen/logrus"
-    "my5G-RANTester/config"
-    control_test_engine "my5G-RANTester/internal/control_test_engine"
-    "sync"
-    "time"
-)
+	"my5G-RANTester/config"
+	control_test_engine "my5G-RANTester/internal/control_test_engine"
+	"sync"
+	"time"
 
+	log "github.com/sirupsen/logrus"
+)
 
 // testing attach and ping for multiple concurrent UEs using TNLAs.
 func TestMultiAttachUesLoadStress(start int, step int, end int, interval int) {
 
-    cfg, err := config.GetConfig()
-    if err != nil {
-        //return nil
-        log.Fatal("Error in get configuration")
-    }
+	cfg, err := config.GetConfig()
+	if err != nil {
+		//return nil
+		log.Fatal("Error in get configuration")
+	}
 
-    // authentication and ping to some concurrent UEs.
-    log.Info("Testing Stress Load: Start[", start, "] Step[", step, "] End[", end ,"] Interval[", interval, " seconds]")
-    log.Info("[CORE]", cfg.AMF.Name, " Core in Testing")
+	// authentication and ping to some concurrent UEs.
+	log.Info("Testing Stress Load: Start[", start, "] Step[", step, "] End[", end, "] Interval[", interval, " seconds]")
+	log.Info("[CORE]", cfg.AMF.Name, " Core in Testing")
 
+	if start > end {
+		testDecrease(start, end, step, interval, cfg)
+		return
+	} else if start < end {
+		testIncrease(start, end, step, interval, cfg)
+		return
+	}
 
-    if start > end {
-        testDecrease(start, end, step, interval, cfg)
-        return
-    } else if start > end {
-        testIncrease(start, end, step, interval, cfg)
-        return
-    }
-
-    testConstant(start, step, interval, cfg)
+	testConstant(start, step, interval, cfg)
 
 }
 
 func testIncrease(start int, end int, step int, interval int, cfg config.Config) {
-    log.Info("Starting Increase Test")
-    ranPort := cfg.GNodeB.ControlIF.Port
-    // Launch several goroutines and increment the WaitGroup counter for each.
-    var wg sync.WaitGroup
-    amount := start
-    imsi_control := 1
-    for amount <= end {
-        log.Info("Launching ", amount, " UEs")
-        for j := 0; j <= amount; j++ {
-            imsi := control_test_engine.ImsiGenerator(imsi_control)
-            imsi_control++
-            wg.Add(1)
-            go attachUeWithTnla(imsi, cfg, int64(imsi_control), &wg, ranPort)
-            ranPort++
-            time.Sleep(10 * time.Millisecond)
-        }
-        amount += step
-        time.Sleep(time.Duration(interval) * time.Millisecond)
-    }
+	log.Info("Starting Increase Test")
+	ranPort := cfg.GNodeB.ControlIF.Port
+	// Launch several goroutines and increment the WaitGroup counter for each.
+	var wg sync.WaitGroup
+	amount := start
+	imsi_control := 1
+	for amount <= end {
+		log.Info("Launching ", amount, " UEs")
+		for j := 0; j <= amount; j++ {
+			imsi := control_test_engine.ImsiGenerator(imsi_control)
+			imsi_control++
+			wg.Add(1)
+			go attachUeWithTnla(imsi, cfg, int64(imsi_control), &wg, ranPort)
+			ranPort++
+			time.Sleep(10 * time.Millisecond)
+		}
+		amount += step
+		time.Sleep(time.Duration(interval) * time.Millisecond)
+	}
 
-    // wait for multiple goroutines.
-    wg.Wait()
+	// wait for multiple goroutines.
+	wg.Wait()
 }
 
 func testDecrease(start int, end int, step int, interval int, cfg config.Config) {
-    log.Info("Starting Decrease Test")
-    ranPort := cfg.GNodeB.ControlIF.Port
-    // Launch several goroutines and increment the WaitGroup counter for each.
-    var wg sync.WaitGroup
-    amount := start
-    imsi_control := 1
-    for amount >= end {
-        for j := 0; j <= amount; j++ {
-            imsi := control_test_engine.ImsiGenerator(imsi_control)
-            imsi_control++
-            wg.Add(1)
-            go attachUeWithTnla(imsi, cfg, int64(imsi_control), &wg, ranPort)
-            ranPort++
-        }
-        amount -= step
-        time.Sleep(time.Duration(interval) * time.Millisecond)
-    }
+	log.Info("Starting Decrease Test")
+	ranPort := cfg.GNodeB.ControlIF.Port
+	// Launch several goroutines and increment the WaitGroup counter for each.
+	var wg sync.WaitGroup
+	amount := start
+	imsi_control := 1
+	for amount >= end {
+		for j := 0; j <= amount; j++ {
+			imsi := control_test_engine.ImsiGenerator(imsi_control)
+			imsi_control++
+			wg.Add(1)
+			go attachUeWithTnla(imsi, cfg, int64(imsi_control), &wg, ranPort)
+			ranPort++
+		}
+		amount -= step
+		time.Sleep(time.Duration(interval) * time.Millisecond)
+	}
 
-    // wait for multiple goroutines.
-    wg.Wait()
+	// wait for multiple goroutines.
+	wg.Wait()
 }
 
 func testConstant(amount int, step int, interval int, cfg config.Config) {
-    log.Info("Starting Constant Test")
-    ranPort := cfg.GNodeB.ControlIF.Port
-    // Launch several goroutines and increment the WaitGroup counter for each.
-    var wg sync.WaitGroup
-    imsi_control := 1
-    for i := 0; i <= step; i++ {
-        for j := 0; j <= amount; j++ {
-            imsi := control_test_engine.ImsiGenerator(imsi_control)
-            imsi_control++
-            wg.Add(1)
-            go attachUeWithTnla(imsi, cfg, int64(imsi_control), &wg, ranPort)
-            ranPort++
-        }
-        time.Sleep(time.Duration(interval) * time.Millisecond)
-    }
+	log.Info("Starting Constant Test")
+	ranPort := cfg.GNodeB.ControlIF.Port
+	// Launch several goroutines and increment the WaitGroup counter for each.
+	var wg sync.WaitGroup
+	imsi_control := 1
+	for i := 0; i <= step; i++ {
+		for j := 0; j <= amount; j++ {
+			imsi := control_test_engine.ImsiGenerator(imsi_control)
+			imsi_control++
+			wg.Add(1)
+			go attachUeWithTnla(imsi, cfg, int64(imsi_control), &wg, ranPort)
+			ranPort++
+		}
+		time.Sleep(time.Duration(interval) * time.Millisecond)
+	}
 
-    // wait for multiple goroutines.
-    wg.Wait()
+	// wait for multiple goroutines.
+	wg.Wait()
 }
